e775de3c844d7610bf66339cc47b78bb56beaa27
Arimahendra1806 <arimahendra1806@gmail.com>
2022-10-21 00:30:42 +0700

    mbenake btn

--- resources/views/user/order/show.blade.php
+++ resources/views/user/order/show.blade.php
@@ -83,7 +83,7 @@
                                         @if ($ndata->getTransaksiFromOrder)
                                             {{-- Jika Transaki Diterima --}}
                                             @if ($ndata->getTransaksiFromOrder->status == 'Diterima')
-                                                <a
+                                                <a class="btn btn-success btn-lg mb-2"
                                                     href="{{ route('cetak.nota', ['id' => $ndata->getTransaksiFromOrder, 'kategori' => $ndata->getProdukFromOrder->kategori]) }}">Cetak
                                                     Sekarang</a>
                                                 <button type="button" class="btn btn-info btn-lg" data-toggle="modal"
@@ -95,7 +95,8 @@
                                                     Pemesanan</button>
                                             @endif
                                         @else
-                                            <a href="{{ route('form.pembayaran', ['id' => $ndata->id]) }}">Bayar
+                                            <a class="btn btn-danger btn-lg mb-2"
+                                                href="{{ route('form.pembayaran', ['id' => $ndata->id]) }}">Bayar
                                                 Sekarang</a>
                                             <button type="button" class="btn btn-info btn-lg" data-toggle="modal"
                                                 data-target="#detail{{ $ndata->id }}">Detail
@@ -272,15 +273,15 @@
         </div>
         </div>
         <!--
-                            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-                                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-                            </script>
-                            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-                                integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-                            </script>
-                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
-                                integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
-                            </script> -->
+                                                        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+                                                            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+                                                        </script>
+                                                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+                                                            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+                                                        </script>
+                                                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+                                                            integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous">
+                                                        </script> -->
 
         @include('sweetalert::alert')
         </body>
