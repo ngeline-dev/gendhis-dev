ace41e5f44a78d8a6bc9b2f372935595458ce491
ngeline-dev <116062314+ngeline-dev@users.noreply.github.com>
2022-10-21 03:52:50 +0700

    Add files via upload

---
+++ resources/views/owner/akun/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+@section('title', 'Edit Data Admin')
+@section('heading', 'Edit Data Admin')
+@section('page', 'Edit Data Admin')
+@section('detailpage', '')
+@section('content')
+    <section class="section">
+        <div class="card">
+            <div class="card-header">
+            </div>
+            <div class="card-body">
+                <form action="{{ route('master-kelolaadmin.update', ['master_kelolaadmin' => $data->id]) }}" method="POST"
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-3">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                                id="name" placeholder="Nama" value="{{ old('name', $data->name) }}"
+                                autofocus>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Role</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control @error('role') is-invalid @enderror"
+                                name="role" id="role" placeholder="Role"
+                                value="{{ old('role', $data->role) }}" autofocus>
+                            @error('role')
+                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control @error('email') is-invalid @enderror"
+                                name="email" id="email" placeholder="Email"
+                                value="{{ old('email', $data->email) }}" autofocus>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="" class="col-sm-2 col-form-label">Status</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control @error('status') is-invalid @enderror"
+                                name="status" id="status" placeholder="Status"
+                                value="{{ old('status', $data->status) }}" autofocus>
+                            @error('status')
+                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div><br></div>
+                    <a href="{{ route('master-kelolaadmin.index') }}" class="btn icon icon-left btn-warning"><i
+                            data-feather="info"></i> Kembali</a>
+                    <button type="submit" class="btn icon icon-left btn-primary col-sm-2"><i data-feather="edit"></i>
+                        Simpan</button>
+                </form>
+            </div>
+        </div>
+    </section>
+@endsection
