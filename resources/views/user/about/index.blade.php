d90149ca60c0dd4f0a822b47e85d81c93f4e0516
ngeline-dev <116062314+ngeline-dev@users.noreply.github.com>
2022-10-27 10:19:01 +0700

    Add files via upload

---
+++ resources/views/user/about/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.appcust')
+
+@section('content')
+<header class="item3 header margin-top-0" style="background-image: url('assets/bg-web.png');  width: 100%; " id="section-home" data-stellar-background-ratio="0.5">
+    <div class="wrapper">
+        <div class="container">
+            <div class="row intro-text align-items-center ">
+                <div class="col-md-10 animated tada"><br><br><br><br>
+                    <h1 class="site-heading site-animate" style="font-size: 67px;">
+                        <strong class="d-block" data-scrollreveal="enter top over 1.5s after 0.1s">CV GENDHIS</strong>
+                    </h1>
+                </div>
+            </div>
+            <div class="item content">
+                <div class="container rightarea">
+                    <div class="col">
+                        <h3 class="numbertext2">CV GENDHIS</h3>
+                        <hr style="background-color: white;">
+                        <p style="color: white; font-family: 'Lucida Console', 'Courier New', monospace; font-size: 15px">
+                        CV GENDHIS adalah .
+                        </p>
+                        <h3 class="numbertext2">E-mail</h3>
+                        <hr style="background-color: white;">
+                        <p style="color: white; font-family: 'Lucida Console', 'Courier New', monospace; font-size: 15px">
+                            gendhis@gmail.com
+                        </p>
+                        <h3 class="numbertext2">Alamat</h3>
+                        <hr style="background-color: white;">
+                        <p style="color: white; font-family: 'Lucida Console', 'Courier New', monospace; font-size: 15px">
+                            Jl. Pesantren No. 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+@endsection
